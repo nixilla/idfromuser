9a19792707b5015e2c3d53b417e8192fa115f8b7
Janusz Slota <janusz.slota@nixilla.com>
2011-10-16 13:13:17 +0100

    Added Google Analytics to track errors Added Google webmasters meta tag added trim to username

--- web/js/main.js
+++ web/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function()
       $('article').append('<div class="loader"><img src="images/loader.gif" alt="Loading" /></div>');
     
       $.ajax({
-        url: "https://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + $('input[name="username"]').val() + "&count=1&callback=?",
+        url: "https://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + jQuery.trim($('input[name="username"]').val()) + "&count=1&callback=?",
         dataType: 'json',
         success: function(data)
         { 
@@ -20,22 +20,25 @@ $(document).ready(function()
             $('article').append('<div class="image"><img src="'+data[0].user.profile_image_url+'" alt="'+data[0].user.screen_name+'" /></div>');
             $('article').append('<div class="profile"><h1>'+data[0].user.name+'</h1><p class="screen-name">@'+data[0].user.screen_name+' <span>'+data[0].user.location+'</span></p><p class="id"><span>'+data[0].user.id+'</span></p></div>');
 
-            console.log(data);
-            console.log(data[0].user.profile_background_color);
             $('body').css('background-color','#'+data[0].user.profile_background_color);
             $('body').css('background-image','url('+data[0].user.profile_background_image_url+')');
+            _gaq.push(['_trackEvent', 'twitter', 'getId', 'Success']);
           }
-          else $('article').append('<p class="error">Success but Twitter returned empty result</p>');
+          else
+          {
+            $('article').append('<p class="error">Success but Twitter returned empty result</p>');
+            _gaq.push(['_trackEvent', 'twitter', 'getId', 'SuccessButEmpty']);
+          }
+            
           
           $('input[type="submit"]').removeAttr('disabled');
         },
         error: function(xhr, ajaxOptions, thrownError)
         {
-          console.log(xhr);
-          console.log(ajaxOptions);
-          console.log(thrownError);
+          _gaq.push(['_trackEvent', 'twitter', 'getId', 'Error']);
           
           $('article').empty();
+
           if(xhr.status == 200)
             $('article').append('<p class="error">Error: Twitter returned empty result</p>');
           else
